ebcf87e0e9b9ba6167fa88550a400e712f718258
Amaan <azafar2000@gmail.com>
2021-06-23 17:13:18 +0530

    bloc observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:cris_attendance/blocs/bloc_observer.dart';
 import 'package:cris_attendance/screens/employee_details.dart';
+import 'package:cris_attendance/styles/app_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
+  Bloc.observer = SimpleBlocObserver();
   runApp(MyApp());
 }
 
@@ -9,13 +13,18 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'CRIS Attendance',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiBlocProvider(
+      providers: [
+        // BlocProvider(
+        //   create: (context) => SubjectBloc(),
+        // ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'CRIS Attendance',
+        theme: AppTheme.theme,
+        home: EmployeeDetailsScreen(),
       ),
-      home: EmployeeDetailsScreen(),
     );
   }
 }
