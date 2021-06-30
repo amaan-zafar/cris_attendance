35c7ebd511e52dcf430d99e50f4b4da36b5a4a94
Amaan <azafar2000@gmail.com>
2021-06-30 15:50:31 +0530

    camera_bloc events and states updated

--- lib/blocs/camera_bloc/camera_event.dart
+++ lib/blocs/camera_bloc/camera_event.dart
@@ -6,3 +6,18 @@ abstract class CameraEvent extends Equatable {
   @override
   List<Object> get props => [];
 }
+
+class ClickImage extends CameraEvent {
+  @override
+  List<Object> get props => [];
+}
+
+class SaveImage extends CameraEvent {
+  @override
+  List<Object> get props => [];
+}
+
+class CompleteAttendance extends CameraEvent {
+  @override
+  List<Object> get props => [];
+}
--- lib/blocs/camera_bloc/camera_state.dart
+++ lib/blocs/camera_bloc/camera_state.dart
@@ -2,9 +2,35 @@ part of 'camera_bloc.dart';
 
 abstract class CameraState extends Equatable {
   const CameraState();
-  
+
   @override
   List<Object> get props => [];
 }
 
 class CameraInitial extends CameraState {}
+
+class ImageNotClicked extends CameraState {
+  const ImageNotClicked();
+  @override
+  List<Object> get props => [];
+}
+
+class ImageClicked extends CameraState {
+  const ImageClicked();
+  @override
+  List<Object> get props => [];
+}
+
+class AttendanceCompleted extends CameraState {
+  const AttendanceCompleted();
+  @override
+  List<Object> get props => [];
+}
+
+class EmployeeError extends CameraState {
+  final String message;
+
+  const EmployeeError({required this.message});
+  @override
+  List<Object> get props => [message];
+}
