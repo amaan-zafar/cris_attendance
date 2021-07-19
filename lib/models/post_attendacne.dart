914d70b5dacc832ff218e57bc333be33c1403bf0
Amaan <azafar2000@gmail.com>
2021-07-19 21:27:39 +0530

    PostAttendance model created

---
+++ lib/models/post_attendacne.dart
@@ -0,0 +1,23 @@
+class PostAttendance {
+  int empId;
+  int slot;
+  String date;
+  String base64;
+  String time;
+
+  PostAttendance(
+      {required this.empId,
+      required this.slot,
+      required this.date,
+      required this.base64,
+      required this.time});
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.empId;
+    data['date'] = this.date;
+    data['img$slot'] = this.base64;
+    data['slot$slot'] = this.time;
+    return data;
+  }
+}
